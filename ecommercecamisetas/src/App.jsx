e351a562247078889f73531f9dbc2c7893397f6b
TomyMontenegro99 <tomymontenegro1999@hotmail.com>
2023-04-04 20:06:08 -0300

    PreEntrega1Montenegro

--- ecommercecamisetas/src/App.jsx
+++ ecommercecamisetas/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <ItemListContainer greeting={'Bienvenidos'}/>
+      <NavBar />
+      <ItemListContainer greeting={"Bienvenidos a Mundo Camisetas"} />
     </div>
   );
 }
